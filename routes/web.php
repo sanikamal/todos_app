cb74dff8f41ef56ad10fdf8a1ab06659afbf917a
Sani Kamal <sanikamal223@gmail.com>
2019-12-04 11:50:21 +0530

    add routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('about', function () {
+//     return view('about');
+// });
+
+Route::get('about','AboutController@index');
