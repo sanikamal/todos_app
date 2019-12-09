90624b651b8070b0ebfbd621966d0af06933b3cd
Sani Kamal <sanikamal223@gmail.com>
2019-12-09 18:49:45 +0530

    add update and delete functionality

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -14,9 +14,9 @@ class TodosController extends Controller
         return view('todos.index')->with("todos",Todo::all());
     }
 
-    public function show($todoId)
+    public function show(Todo $todo)
     {
-        $todo=Todo::find($todoId);
+        // $todo=Todo::find($todoId);
             // dd($todoId);
             return view('todos.show')->with('todo',$todo);
     }
@@ -36,6 +36,42 @@ class TodosController extends Controller
       $todo->description = $data['description'];
       $todo->completed = false;
       $todo->save();
+      session()->flash('success', 'Todo created successfully.');
+      return redirect('/todos');
+    }
+
+    public function edit(Todo $todo)
+    {
+    //   $todo = Todo::find($todoId);
+      return view('todos.edit')->with('todo', $todo);
+    }
+    public function update($todoId)
+    {
+      $this->validate(request(), [
+        'name' => 'required|min:6|max:12',
+        'description' => 'required'
+      ]);
+      $data = request()->all();
+      $todo = Todo::find($todoId);
+      $todo->name = $data['name'];
+      $todo->description = $data['description'];
+      $todo->save();
+      session()->flash('success', 'Todo updated successfully.');
+      return redirect('/todos');
+    }
+    public function destroy(Todo $todo)
+    {
+    //   $todo = Todo::find($todo);
+      $todo->delete();
+      session()->flash('success', 'Todo deleted successfully.');
+      return redirect('/todos');
+    }
+
+    public function complete(Todo $todo)
+    {
+      $todo->completed = true;
+      $todo->save();
+      session()->flash('success', 'Todo completed successfully.');
       return redirect('/todos');
     }
 }
